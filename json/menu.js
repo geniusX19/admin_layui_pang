167edb56f88eb45c97278636187a28c4d0447158
huangjunjie666@sina.cn <huangjunjie666@sina.cn>
2019-07-02 15:38:31 +0800

    1

---
+++ json/menu.js
@@ -0,0 +1,129 @@
+{
+    "code": 0,
+    "msg": "ok",
+    "data": [
+        {
+            "title": "首页",
+            "icon": "layui-icon-home",
+            "href": "/"
+        },
+        {
+            "title": "系统配置",
+            "icon": "layui-icon-unorderedlist",
+            "childs": [
+                {
+                    "title": "常规配置",
+                    "href": "/list/table/id=1"
+                },
+                {
+                    "title": "第三方配置",
+                    "href": "/list/card"
+                }
+            ]
+        },
+        {
+            "title": "用户管理",
+            "icon": "layui-icon-container",
+            "childs": [
+                {
+                    "title": "普通用户",
+                    "href": "/_userAdmin/index"
+                },
+                {
+                    "title": "商家",
+                    "href": "/_business/index"
+                },
+                {
+                    "title": "配送员",
+                    "href": "/_distributor/index"
+                },
+                {
+                    "title": "作者",
+                    "href": "/chart/index"
+                },
+                {
+                    "title": "角色管理",
+                    "href": "/chart/index"
+                }
+            ]
+        },
+        {
+            "title": "内容管理",
+            "icon": "layui-icon-file-exception",
+            "childs": [
+                {
+                    "title": "Banner管理",
+                    "href": "/form/basic"
+                }
+            ]
+        },
+        {
+            "title": "分类管理",
+            "icon": "layui-icon-error",
+            "childs": [
+                {
+                    "title": "热门分类",
+                    "href": "/exception/403"
+                }
+            ]
+        },
+        {
+            "title": "文章管理",
+            "icon": "layui-icon-experiment",
+            "notice": 3,
+            "childs": [
+                {
+                    "title": "文章列表",
+                    "href": "/module/admin"
+                },
+                {
+                    "title": "文章分类",
+                    "href": "/module/helper"
+                }
+            ]
+        },
+        {
+            "title": "商家管理",
+            "icon": "layui-icon-experiment",
+            "notice": 3,
+            "childs": [
+                {
+                    "title": "商家列表",
+                    "href": "/module/admin"
+                }
+            ]
+        },
+        {
+            "title": "资金流水帐",
+            "icon": "layui-icon-experiment",
+            "notice": 3,
+            "childs": [
+                {
+                    "title": "商家资金流",
+                    "href": "/module/admin"
+                },
+                {
+                    "title": "配送员资金流",
+                    "href": "/module/admin"
+                },
+                {
+                    "title": "作者资金流",
+                    "href": "/module/admin"
+                },
+                {
+                    "title": "提现申请",
+                    "href": "/module/admin"
+                },
+                {
+                    "title": "提现记录",
+                    "href": "/module/admin"
+                }
+            ]
+        },
+        {
+            "title": "图标",
+            "icon": "layui-icon-star",
+            "href": "/icon/index"
+        }
+    ]
+}
